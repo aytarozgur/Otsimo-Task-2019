a2d3d677b0a67ef83cb9c41142511128cdba4ac1
Ozgur Aytar <zrhaytar@gmail.com>
2019-02-19 01:07:55 +0300

    variables

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ const Hapi = require('hapi');
 
 const server = new Hapi.Server();
 server.connection({ port: 8080 });
+
+var moves = [
+  'rock',
+  'paper',
+  'scissors'
+];
+
+var newGame = '/newGame';
+var endpoints = moves.map( move => {
+  return `/${move}`;
+});
